0a9eb86a030d68949d48025d3e10d7152bd43fcf
SHA <shapai0621@gmail.com>
2019-09-26 01:50:56 +0800

    lab02_openLight press

--- lab02_openLight.py
+++ lab02_openLight.py
@@ -10,14 +10,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
+ledState = False
 try:
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-            GPIO.output(LED_PIN, GPIO.LOW)
-            print('botton')
+            ledState = not ledState
+            GPIO.output(LED_PIN, ledState)
+            print('Press')
         time.sleep(0.1)
-        else:
-            GPIO.output(LED_PIN, GPIO.HIGH)
+
 except KeyboardInterrupt:
     print('close')
 finally:
