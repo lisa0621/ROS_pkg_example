d1bf8b946cf5bd5cc1289297a327d0a7224fa5e6
SHA <shapai0621@gmail.com>
2019-09-26 00:35:41 +0800

    lab02_openLight

--- lab02_openLight.py
+++ lab02_openLight.py
@@ -3,22 +3,22 @@ import time
 import RPi.GPIO as gpio
 import rospy
 
-BUTTON_PIN = 17
-
 gpio.setmode(gpio.BCM)
-gpio.setup(BUTTON_PIN, gpio.IN, pull_up_down=gpio.PUD_UP)  # button
+gpio.setup(17, gpio.IN)  # button
 gpio.setup(18, gpio.OUT)  # led
 print('pwm start')
-
+gpio.output(18, gpio.LOW)
 try:
     while True:
-        print('light off')
-        gpio.output(18, gpio.LOW)
+        #print('light off')
+        #gpio.output(18, gpio.LOW)
 
-        if gpio.input(BUTTON_PIN) == gpio.LOW:
+        if gpio.input(17) == gpio.HIGH:
             time.sleep(0.1)
             print('light on')
             gpio.output(18, gpio.HIGH)
+        else:
+            gpio.output(18, gpio.LOW)
 except KeyboardInterrupt:
     print('close')
 finally:
