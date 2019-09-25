b07c70508d6bc0c5804f1ed3f2210e60ccceccc5
SHA <shapai0621@gmail.com>
2019-09-26 01:01:00 +0800

    lab02

--- lab02_openLight.py
+++ lab02_openLight.py
@@ -3,21 +3,18 @@ import time
 import RPi.GPIO as GPIO
 import rospy
 
-BUTTON_PIN = 17
-LED_PIN = 18
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
-GPIO.setup(LED_PIN, GPIO.OUT)  # led
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
+GPIO.setup(18, GPIO.OUT)  # led
 
 try:
     while True:
-        button_state = GPIO.input(BUTTON_PIN)
+        button_state = GPIO.input(17)
         if button_state == False:
-            GPIO.output(LED_PIN, True)
+            GPIO.output(18, True)
             print('Button Pressed...')
             time.sleep(0.2)
         else:
-            GPIO.output(LED_PIN, False)
+            GPIO.output(18, False)
 except:
     GPIO.cleanup()
